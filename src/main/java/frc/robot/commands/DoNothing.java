c471641ba154e83735c26d948a58b727d28ab6f6
Brian Kircher <bckircher@yahoo.com>
2021-10-11 19:56:00 -0500

    Remove unneeded overrides in the DoNothing command

--- src/main/java/frc/robot/commands/DoNothing.java
+++ src/main/java/frc/robot/commands/DoNothing.java
@@ -19,25 +19,4 @@ public class DoNothing extends CommandBase {
    */
   public DoNothing() {
   }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
 }
